3c13f97f1944a7670d4c23c10f7fa9fedcba64b0
Grant <granttlalonde@gmail.com>
2020-02-10 22:56:02 -0600

    First

---
+++ Controllers/RecipeHomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+
+
+namespace HomeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RecipeHomeController : ControllerBase
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public RecipeHomeController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+            return "Culinary Companion Home API";
+        }
+    }
+}
