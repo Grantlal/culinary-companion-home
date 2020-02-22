adbb38178b69109d327878881aeaa69ba3753a7f
Grant <granttlalonde@gmail.com>
2020-02-22 15:10:50 -0600

    Added tenchnique response

---
+++ Controllers/TechniqueHomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using HomeAPI.Requests;
+
+namespace HomeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TechniqueHomeController : ControllerBase
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public TechniqueHomeController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<string> GetTechniqueAsync(string keyword)
+        {
+            try
+            {
+                var exampleRequester = new HttpRequests();
+                var url = "https://culinary-companion-technique.herokuapp.com/techniques?searchTag=";
+                url += keyword;
+                return await exampleRequester.GetAsync(url);
+            }
+            catch
+            {
+                return "Nope";
+            }
+        }
+    }
+}
---
+++ Requests/HttpRequests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace HomeAPI.Requests
+{
+    public class HttpRequests 
+    {
+        public HttpRequests() { }
+        public async Task<string> GetAsync(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
