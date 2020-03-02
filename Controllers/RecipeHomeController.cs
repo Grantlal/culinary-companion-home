d44e8edff37fe2ffcd0a9f4f91c35a9f4db93d51
Grant <granttlalonde@gmail.com>
2020-03-02 12:18:32 -0600

    added parameter to recipe post request

--- Controllers/RecipeHomeController.cs
+++ Controllers/RecipeHomeController.cs
@@ -16,19 +16,19 @@ namespace HomeAPI.Controllers
     {
         private static readonly HttpClient client = new HttpClient();
 
-        [HttpGet]
-        public async Task<string> GetAsync()
+        [HttpPost]
+        public async Task<string> PostAsync(string value)
         {
             try
             {
                 var recipeRequester = new HttpRequests();
                 var uri = "https://u7bwesiqoc.execute-api.us-east-1.amazonaws.com/Prod/search/";
-                var body = "{\"query\":\"beef\"}";
+                var body = value;
                 return await recipeRequester.PostAsync(uri, body, "JSON");
             }
-            catch
+            catch (Exception e)
             {
-                return "Culinary Companion Home API";
+                return e.ToString();
             }
         }
     }
