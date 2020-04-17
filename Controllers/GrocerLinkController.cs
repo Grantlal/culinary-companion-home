0afab1404c91cc4e127e8b30480ae20d2de438d5
Grant <granttlalonde@gmail.com>
2020-04-17 13:06:40 -0500

    added grocerLink

---
+++ Controllers/GrocerLinkController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeAPI.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GrocerLinkController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> GetAsync(string keyword)
+        {
+            try
+            {
+                var exampleRequester = new HttpRequests();
+                var url = "https://culinarycompaniongrocerlink.azurewebsites.net/grocerlink?keyword=";
+                url += keyword;
+                return await exampleRequester.GetAsync(url);
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+    }
+}
